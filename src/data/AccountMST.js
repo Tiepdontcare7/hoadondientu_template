8478d38d59f6759384ccc360cb63c6d0b1695b1d
Tiep Pham <payvip1802@gmail.com>
2024-03-04 08:44:43 +0700

    random account

--- src/data/AccountMST.js
+++ src/data/AccountMST.js
@@ -1,38 +1,402 @@
 export const Account = [
     {
-        "username": "EritreaAdeline",
-        "password": "06042001@Adua."
+        username: "EritreaAdeline",
+        password: "06042001@Adua.",
     },
     {
-        "username": "IranLily",
-        "password": "06042001@Adua."
+        username: "IranLily",
+        password: "06042001@Adua.",
     },
     {
-        "username": "EthiopiaDennis",
-        "password": "06042001@Adua."
+        username: "EthiopiaDennis",
+        password: "06042001@Adua.",
     },
     {
-        "username": "CuraçaoNell",
-        "password": "06042001@Adua."
+        username: "CuraçaoNell",
+        password: "06042001@Adua.",
     },
     {
-        "username": "MicronesiaCaroline",
-        "password": "06042001@Adua."
+        username: "MicronesiaCaroline",
+        password: "06042001@Adua.",
     },
     {
-        "username": "IsleofManLydia",
-        "password": "06042001@Adua."
+        username: "IsleofManLydia",
+        password: "06042001@Adua.",
     },
     {
-        "username": "MongoliaHoward",
-        "password": "06042001@Adua."
+        username: "MongoliaHoward",
+        password: "06042001@Adua.",
     },
     {
-        "username": "LiechtensteinTeresa",
-        "password": "06042001@Adua."
+        username: "LiechtensteinTeresa",
+        password: "06042001@Adua.",
     },
     {
-        "username": "LaosMable",
-        "password": "06042001@Adua."
-    }
-]
+        username: "LaosMable",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "ZimbabweGavin",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "ZambiaHannah",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "ZanzibarIsaac",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "YemenJessica",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "VietnamKylie",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "VenezuelaLiam",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "UzbekistanMolly",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "UruguayNoah",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "UnitedStatesOliver",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "UnitedKingdomPenelope",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "UkraineQuinn",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "UgandaRiley",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "TuvaluSophia",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "TurkmenistanThomas",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "TurkeyUrsula",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "TunisiaVincent",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "TrinidadandTobagoWendy",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "TongaXander",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "TokelauYvonne",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "Timor-LesteZachary",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "ThailandAbigail",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "TajikistanBenjamin",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "TaiwanCharlotte",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SyriaDavid",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SwitzerlandEmma",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SwedenFinn",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SwazilandGrace",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SurinameHenry",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SudanIris",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SriLankaJack",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SpainKate",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SouthSudanLeo",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SouthKoreaMia",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SouthAfricaNathan",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SomaliaOlivia",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SolomonIslandsPaul",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SloveniaQuinn",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SlovakiaRiley",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SingaporeSophia",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SierraLeoneThomas",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SeychellesUrsula",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SerbiaVincent",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SenegalWendy",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SaudiArabiaXander",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SamoaYvonne",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SaintVincentandtheGrenadinesZachary",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SaintLuciaAbigail",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "SaintKittsandNevisBenjamin",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "RwandaCharlotte",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "RussiaDavid",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "RomaniaEmma",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "QatarFinn",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "PortugalGrace",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "PolandHenry",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "PhilippinesIris",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "PeruJack",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "ParaguayKate",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "PanamaLeo",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "PakistanMia",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "OmanNathan",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "NorwayOlivia",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "NorthKoreaPaul",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "NigeriaQuinn",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "NigerRiley",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "NicaraguaSophia",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "NewZealandThomas",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "NetherlandsUrsula",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "NepalVincent",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "NamibiaWendy",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "MyanmarXander",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "MozambiqueYvonne",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "MoroccoZachary",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "MontenegroAbigail",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "MongoliaBenjamin",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "MonacoCharlotte",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "MoldovaDavid",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "MexicoEmma",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "MauritiusFinn",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "MauritaniaGrace",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "MarshallIslandsHenry",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "MaltaIris",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "MaliJack",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "MaldivesKate",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "MalaysiaLeo",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "MalawiMia",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "MadagascarNathan",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "MacedoniaOlivia",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "LuxembourgPaul",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "LithuaniaQuinn",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "LiechtensteinRiley",
+        password: "06042001@Adua.",
+    },
+    {
+        username: "LibyaSophia",
+        password: "06042001@Adua.",
+    },
+];
